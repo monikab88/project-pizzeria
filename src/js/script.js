90ff903b75c37444f52aee28997a356cf6a7c99d
Monika Bielicz <bieliczm@gmail.com>
2023-01-16 00:37:12 +0100

    init menu for in

--- src/js/script.js
+++ src/js/script.js
@@ -68,8 +68,12 @@
     },
 
     initMenu: function(){
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
+      const thisApp = this;
+      console.log('thisApp.data:',thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
     },
 
     init: function(){
