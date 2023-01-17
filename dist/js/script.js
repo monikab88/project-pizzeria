e18fd7a9bcbb031ae6a0809842f303536146305a
Monika Bielicz <bieliczm@gmail.com>
2023-01-17 21:02:45 +0100

    accordion3

--- dist/js/script.js
+++ dist/js/script.js
@@ -85,29 +85,29 @@
     initAccordion(){
       const thisProduct = this;
       
-    /* find the clickable trigger (the element that should react to clicking) */
-    const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
-    /* START: add event listener to clickable trigger on event click */
-    clickableTrigger.addEventListener('click', function(event) {
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
     
-      /* prevent default action for event */
-      event.preventDefault();
+        /* prevent default action for event */
+        event.preventDefault();
       
-      /* find active product (product that has active class) */
-      const activeProduct = document.querySelector(select.all.menuProductsActive);
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
       
-      /* if there is active product and it's not thisProduct.element, remove class active from it */
-      if (activeProduct !== null && activeProduct !== thisProduct.element){
-        activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
-      }
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct !== null && activeProduct !== thisProduct.element){
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
       
-      /* toggle active class on thisProduct.element */
-       thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        /* toggle active class on thisProduct.element */
+         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
    
       });
+    }
   }
-}
   
   const app = {
     initData: function(){
--- src/js/script.js
+++ src/js/script.js
@@ -103,7 +103,7 @@
         }
       
         /* toggle active class on thisProduct.element */
-         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
    
       });
     }
