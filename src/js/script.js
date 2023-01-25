9e0601ece159b1ae3725806e821ba2b00a211c87
Monika Bielicz <bieliczm@gmail.com>
2023-01-25 04:16:22 +0100

    processOrder full

--- src/js/script.js
+++ src/js/script.js
@@ -167,12 +167,25 @@
           console.log(optionId, option);
         }
       }
-
+   
+        // check if there is param with a name of paramId in formData and if it includes optionId
+        if(formData[paramId] && formData[paramId].includes(optionId)) {
+         // check if the option is not default
+          if(!option.default) {
+            // add option price to price variable
+            price += option.price;
+          }
+        } else {
+         // check if the option is default
+         if(!option.default) {
+            // reduce price variable
+            price -= option.price;
+         }
+        }
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
-    }
-
   }
+}
   
   
   const app = {
