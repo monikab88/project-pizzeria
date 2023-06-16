e85de8a7bc024884621d722aacc0fa2360b4ce2b
Monika Bielicz <bieliczm@gmail.com>
2023-06-16 07:41:35 +0200

    components3

--- dist/js/components/Cart.js
+++ dist/js/components/Cart.js
@@ -1,5 +1,5 @@
 import {select, classNames, settings, templates} from '../settings.js';
-import CartProduct from './components/CartProduct.js';
+import CartProduct from './CartProduct.js';
 import utils from '../utils.js';
 
 class Cart{
