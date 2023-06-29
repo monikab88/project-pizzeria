e8bbf66bf6b44d5059b2a4648d952d20632b80b5
Monika Bielicz <bieliczm@gmail.com>
2023-06-29 21:01:35 +0200

    booking promise

--- dist/js/components/Booking.js
+++ dist/js/components/Booking.js
@@ -16,20 +16,26 @@ class Booking{
   getData(){
     const thisBooking = this;
 
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
     const params = {
       booking:[
-        settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate),
-        settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate),
+        startDateParam,        
+        endDateParam,
       ],
       eventsCurrent: [
-
+        settings.db.notRepeatParam,
+        startDateParam,        
+        endDateParam,
       ],
       eventsRepeat: [
-
+        settings.db.repeatParam,
+        endDateParam,
       ],
     };
 
-    console.log('getData params', params);
+    // console.log('getData params', params);
 
     const urls = {
       booking:            settings.db.url + '/' + settings.db.booking 
@@ -40,9 +46,30 @@ class Booking{
                           + '?' + params.eventsRepeat.join ('&'),
     };
 
-    console.log('getData urls', urls);
+    //console.log('getData urls', urls);
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function(allResponses){
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
 
+        return Promise.all ([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]){
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+      });
   }
+
   render(element){
     const thisBooking = this;
   
