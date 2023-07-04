bd2842424304e858cbe832bb426087b3010f51f4
Monika Bielicz <bieliczm@gmail.com>
2023-07-04 11:04:31 +0200

    send booking2

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -62,6 +62,11 @@ export const select = {
     peopleAmount: '.people-amount',
     hoursAmount: '.hours-amount',
     tables: '.floor-plan .table',
+    floorPlan: '.floor-plan',
+    phone: '[name="phone"]',
+    address: '[name="address"]', 
+    form: '.booking-form',
+    starterCheckbox: '.starter',
   },
   nav: {
     links: '.main-nav a',
@@ -79,6 +84,7 @@ export const classNames = {
   booking: {
     loading: 'loading',
     tableBooked: 'booked',
+    tableSelected: 'selected',
   },
   nav: {
     active: 'active',
